4984cb446cf73c17fcc775b49856e1d5c8d16738
Ester Maria <estermariasouza2005@gmail.com>
2025-04-05 22:04:44 -0300

    Primeiro Commit

---
+++ ex1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+def load_sms_spam_dataset(data_path, seed=123):
+    """
+    Loads the dataset of emails that are categorized into spam and ham
+    # Arguments
+        data_path: string, path for the data directory
+        seed: int, seed for randomizer
+    # Returns 
+    """
+    sms_data_path = os.path.join(data_path, 'SMSSpamCollection')
+
+
+load_sms_spam_dataset("C:\Users\ester\OneDrive\Documentos\QuintoPeríodo\Treinamento LapPi\smsspamcollection")
